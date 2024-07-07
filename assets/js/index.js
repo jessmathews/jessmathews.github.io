7410d587c7cb5ac95a7a213a0c0e2fbb90f6945b
Jess <jessmathews190@gmail.com>
2024-07-07 20:19:12 +0530

    first commit to dev

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.querySelector(".coloured").innerText;
-    const coloredText = text.split("").map((char, index) => {
-        return `<span>${char}</span>`;
-    }).join("");
-    document.querySelector(".coloured").innerHTML = coloredText;
-});
