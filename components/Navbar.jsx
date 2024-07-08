e9e6e0d5ac8f93670d07c4d4314e834ffccc18d6
Jess <81862618+jessmathews@users.noreply.github.com>
2024-07-08 16:34:31 +0000

    Update Navbar links with transition effects

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,20 +17,20 @@ const Navbar = () => {
       <div className="z-1 flex justify-center items-center relative h-full px-3">
 
         <Link href="/" className="absolute z-[1002]" onClick={() => onUpdateActiveLink('#')}>
-          <div className="text-2xl font-semibold">Jess<span className="text-accent jumping-char ">.</span></div></Link>
+          <div className="text-2xl font-semibold hover:text-accent transition-all">Jess<span className="text-accent jumping-char ">.</span></div></Link>
 
         <nav className="w-full h-full float-right relative">
           <div className="flex items-center w-full h-full justify-between flex-row ">
 
             <nav className="h-full gap-3 flex-nowrap flex justify-center items-center">
-              <Link href="/#skills" className={activeLink === 'skills' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent'} onClick={() => onUpdateActiveLink('skills')}>skills</Link>
-              <Link href="/#projects" className={activeLink === 'projects' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent'} onClick={() => onUpdateActiveLink('projects')}>projects</Link>
-              <Link href="/contact" className={activeLink === 'contact' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent'} onClick={() => onUpdateActiveLink('contact')}>contact</Link>
+              <Link href="/#skills" className={activeLink === 'skills' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent transition-all'} onClick={() => onUpdateActiveLink('skills')}>skills</Link>
+              <Link href="/#projects" className={activeLink === 'projects' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent transition-all'} onClick={() => onUpdateActiveLink('projects')}>projects</Link>
+              <Link href="/contact" className={activeLink === 'contact' ? 'text-accent opacity-100 link-button' : 'link-button opacity-75 hover:text-accent transition-all'} onClick={() => onUpdateActiveLink('contact')}>contact</Link>
             </nav>
             <nav className="h-full gap-3 flex-nowrap flex justify-center items-center text-4xl">
-              <Link href="https://github.com/jessmathews" target="_blank" className="hover:text-accent"><FaGithub /></Link>
-              <Link href="https://www.linkedin.com/in/jessmathews24/" target="_blank" className="hover:text-accent"><FaLinkedin /></Link>
-              <Link href="https://x.com/i_am_jezz" target="_blank" className="hover:text-accent"><FaSquareXTwitter /></Link>
+              <Link href="https://github.com/jessmathews" target="_blank" className="hover:text-accent transition-all"><FaGithub /></Link>
+              <Link href="https://www.linkedin.com/in/jessmathews24/" target="_blank" className="hover:text-accent transition-all"><FaLinkedin /></Link>
+              <Link href="https://x.com/i_am_jezz" target="_blank" className="hover:text-accent transition-all"><FaSquareXTwitter /></Link>
             </nav>
           </div>
         </nav>
